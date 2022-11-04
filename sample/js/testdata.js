b1f189f2702c41edfb4a141f5893122e60fa0b7d
cycau <116693177+cycau@users.noreply.github.com>
2022-11-04 11:27:18 +0900

    Add files via upload

---
+++ sample/js/testdata.js
@@ -0,0 +1,21 @@
+var initData = {};
+initData.key1 = "init.";
+initData.datalist = [];
+initData.datalist.push({colA:"colAval1", colB:"colBval1", colC:"colCval1", colD:"colDval1"});
+initData.datalist.push({colA:"colAval2", colB:"colBval2", colC:"colCval2", colD:"colDval2"});
+initData.datalist.push({colA:"colAval3", colB:"colBval3", colC:"colCval3", colD:"colDval3"});
+
+
+var refreshData = {};
+refreshData.key1 = "refreshed.";
+refreshData.datalist = [];
+refreshData.datalist.push({colA:"RefAval1", colB:"RefBval1", colC:"RefCval1", colD:"RefDval1"});
+refreshData.datalist.push({colA:"RefAval2", colB:"RefBval2", colC:"RefCval2", colD:"RefDval2"});
+refreshData.datalist.push({colA:"RefAval3", colB:"RefBval3", colC:"RefCval3", colD:"RefDval3"});
+refreshData.datalist.push({colA:"RefAval4", colB:"RefBval4", colC:"RefCval4", colD:"RefDval4"});
+refreshData.datalist.push({colA:"RefAval5", colB:"RefBval5", colC:"RefCval5", colD:"RefDval5"});
+
+var modalData = {};
+modalData.datalist = [];
+modalData.datalist.push({colA:"ModAval1", colB:"ModBval1", colC:"ModCval1"});
+modalData.datalist.push({colA:"ModAval2", colB:"ModBval2", colC:"ModCval2"});
