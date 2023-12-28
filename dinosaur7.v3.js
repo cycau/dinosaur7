769936145ddd84c93c71c241f022a8834ad5c8fa
cycau <116693177+cycau@users.noreply.github.com>
2023-12-28 18:42:07 +0900

    Update dinosaur7.v3.js

--- dinosaur7.v3.js
+++ dinosaur7.v3.js
@@ -1,4 +1,6 @@
-/**
+/*******************************
+ * [Dinosaur7 SPA]
+ * 
  * /page/path1/pageA.html?param1=p1
  * /layout/layoutA.html
  * /component/compA.html
@@ -10,155 +12,321 @@
  * 				-> compA
  * 				-> compB
  * 				-> compC
- */
-class Dinosaur7 {
+ *******************************/
+let _D7_PAGE_BASE = null;
+let _D7_DEFAULT_LAYOUT = null;
+class D7App {
 	constructor() {
 		this._CACHE_PAGE = {};
 		this._CACHE_LAYOUT = {};
+		this._GLOBAL = {};
 	}
-	route = function(url) {
-		let page = this.getPage(url);
-		if (page) {
-			page.show(url);
-			return;
+
+	init = function(opt) {
+		this._opt = opt;
+		_D7_PAGE_BASE = opt.pageBase || '';
+		_D7_DEFAULT_LAYOUT = opt.defaultLayout || '/';
+
+		const pageInfo = D7Util.parseUrl(opt.topPage || window.location.href);
+		this._init_page = pageInfo.query.d7Page || pageInfo.query.d7page || pageInfo.path;
+
+		const preload = opt.preload || [];
+		for (const pageUrl of preload) {
+			const pageInfo = D7Util.parseUrl(pageUrl);
+			this.loadPage(pageInfo.path); // 並列
 		}
-		showPage(url);
 	}
-	showPage = function(url) {
-		const urlInfo = D7Util.parseUrl(url);
-		let d7Page = this._CACHE_PAGE[urlInfo.url];
-		if (d7Page) {
-			d7Page.show();
-			return;
+
+	start = function() {
+		this._APP_ELEM = document.querySelector('body[d7App]');
+		if (!this._APP_ELEM) console.error('no [d7App] defined in root html.');
+
+		this.route(this._init_page);
+	}
+
+	route = async function(pageUrl) {
+		const pageInfo = D7Util.parseUrl(pageUrl);
+
+		let d7Page = this._CACHE_PAGE[pageInfo.path];
+		if (!d7Page) {
+			d7Page = await D7Page.load(pageInfo.path)
+			this._CACHE_PAGE[pageInfo.path] = d7Page;
 		}
 
-		const d7App = this;
-		D7Page.load(urlInfo.url)
-		.then((d7Page) => {
-			d7App._CACHE_PAGE[d7Page.url] = d7Page;
-			d7App.currPage = d7Page;
-
-			let d7Layout = d7App._CACHE_LAYOUT[d7Page.layoutUrl];
-			if (d7Layout) {
-				d7Page.setLayout(d7Layout);
-				return d7Layout;
-			}
-
-			return d7Page.loadLayout();
-		}).then((d7Layout) => {
-			d7App._CACHE_LAYOUT[d7Layout.url] = d7Layout;
-
-			d7App.currPage.show();
-		})
+		await d7Page.render(pageInfo.query);
 	}
-	addPage = function(url, d7Page) {
-		this._CACHE_PAGE[url] = d7Page;
-	}
-	addLayout = function(url, d7Layout) {
-		this._CACHE_LAYOUT[url] = d7Layout;
+
+	storeData = function(key, val) {
+		if (val === 'undefined') {
+			return this._GLOBAL[key];
+		}
+		this._GLOBAL[key] = val;
 	}
 }
+
+/*******************************
+ * CONCEPT
+ *******************************/
 class D7Page {
-	constructor() {
+	constructor(path) {
+		this._PATH = path;
+		this._PROP = null;
+		this._SELF_COMP = null;
 	}
 
-	static load = async (url) => {
-		console.log('loading page...' + url);
-		let htmlText = await D7Api.loadComp(url);
-		const d7Page = D7Page.compile(htmlText);
-		d7Page.url = url;
+	static load = async (path) => {
+		console.log('loading page...' + path);
+
+		const d7Page = new D7Page(path);
+		d7Page._SELF_COMP = await D7Component.load(path);
+
 		return d7Page;
 	}
 
-	static compile = (htmlText) => {
-		console.log('compiling page...');
-		const d7Page = new D7Page(htmlText);
-		return d7Page;
+	render = async function(property) {
+		console.log('redering page...');
+		const layoutInfo = this._SELF_COMP.getLayoutInfo(property);
+
+		let d7Layout = d7App._CACHE_LAYOUT[layoutInfo.path];
+		if (!d7Layout) {
+			d7Layout = await D7Layout.load(layoutInfo.path, d7App._APP_ELEM);
+			d7App._CACHE_LAYOUT[layoutInfo.path] = d7Layout;
+		}
+
+		d7Layout.render(layoutInfo.query);
+		const mountPoint = d7Layout.getPageLocation();
+		const currPath = mountPoint.getAttribute('d7CurrLayout');
+		if (currPath === this._PATH && D7Util.equals(property, this._PROP)) return;
+
+		const virtualDom = this._SELF_COMP.virtualRender(property);
+		mountPoint.append(virtualDom);
 	}
 
-	setLayout = (d7Layout) => {
-		this.layout = d7Layout;
-		return this;
-	}
-
-	loadLayout = async () => {
-		const d7Layout = await D7Layout.load();
-		this.layout = d7Layout;
-		return d7Layout;
-	}
-
-	show = function() {
-		console.log('showing page...');
-		return this;
-	}
 }
 
 class D7Layout {
-
-	constructor() {
-		this.layout = 'layout';
+	constructor(path, mountPoint) {
+		this._PATH = path;
+		this._PROP = null;
+		this._SELF_COMP = null;
+		this._MOUNT_POINT = mountPoint;
+		this._MOUNT_PAGE = null;
 	}
 
-	static load = async (url) => {
-		console.log('loading layout...' + url);
+	static load = async (path, mountPoint) => {
+		console.log('loading layout...' + path);
+		const d7Layout = new D7Layout(path, mountPoint);
+		if (path === '/') {
+			d7Layout._MOUNT_PAGE = mountPoint;
+			return d7Layout;
+		}
 
-		const d7Layout = D7Layout.compile('');
-		await d7Layout.loadComponents();
+		d7Layout._SELF_COMP = await D7Component.load(path);
 		return d7Layout;
 	}
 
-	static compile = function(htmlText) {
-		console.log('compiling layout...');
-		const d7Layout = new D7Layout();
-		return d7Layout;
+	render = function(property) {
+		console.log('redering layout...');
+		if (this._PATH === '/')  return;
+
+		const currPath = this._MOUNT_ELEMENT.getAttribute('d7CurrLayout');
+		if (currPath === this._PATH && D7Util.equals(property, this._PROP)) return;
+
+		const virtualDom = this._SELF_COMP.renderComp(property);
+		this._MOUNT_POINT.append(virtualDom);
+		this._MOUNT_POINT.setAttribute('d7CurrLayout', this._PATH);
+		this._MOUNT_PAGE = virtualDom.querySelector('d7Page');
 	}
 
-	loadComponents = async () => {
-		console.log('loading components...');
-		this.compileComponents();
-		return this;
-	}
-
-	compileComponents = function() {
-		console.log('compiling components...');
-	}
-
-	show = function() {
-		this.layout.show();
-		console.log('showing page...');
+	getPageLocation = function() {
+		return this._MOUNT_PAGE;
 	}
 }
-class D7Compiler {
+
+/*******************************
+ * COMPONENT
+ *******************************/
+class D7Component {
+
+	constructor(path) {
+		this._PATH = path;
+		this._PROP = null;
+		this._DOM = null;
+
+		this._CHILDREN = {};
+		this._RENDERER = null;
+		this._EVENTS = null;
+	}
+
+	static load = async function(path) {
+		const d7Comp = new D7Component(path);
+		d7Comp._RENDERER = await D7Renderer.create(_D7_PAGE_BASE + path);
+		return d7Comp;
+	}
+
+	virtualRender = function(property) {
+		if (D7Util.equals(property, this._PROP)) return this._DOM;
+
+		const virtualDom = this._RENDERER.render(property);
+		var elements = virtualDom.querySelectorAll('d7Comp');
+		for (const childRoot of elements) {
+			const elemUrl = D7Util.parseUrl(childRoot.getAttribute('d7Comp'));
+			
+			let childComp = this._CHILDREN[elemUrl.path]
+			if (!childComp) {
+				childComp = D7Component.load(elemUrl.path);
+				this._COMPONENTS[elemUrl.path] = childComp;
+				continue;
+			}
+
+			childRoot.append(childComp.render(elemUrl.query));
+		}
+
+		this._PROP = property
+		this._DOM = virtualDom;
+		return virtualDom;
+	}
+
+	getLayoutInfo = function(property) {
+		const layoutElem = this._RENDERER.renderLayoutElem(property);
+		if (!layoutElem) return {path:_D7_DEFAULT_LAYOUT, query:{}};
+
+		return D7Util.parseUrl(layoutElem.getAttribute('d7Layout'));
+	}
+
+	render = function(modelData, selector) {
+	}
+
+	collect = function(selector) {
+		return {};
+	}
+
+	bindEvent = function(selector, eventName, eventFunc) {
+	}
+
+	show = function(visible) {
+		this._DOM.show(visible);
+	}
+
 }
+
+/*******************************
+ * RENDERER
+ *******************************/
+const _D7_CONTAINER_TYPE = {
+	"CAPTION": "table", 
+	"THEAD": "table",
+	"TFOOT": "table",
+	"TBODY": "table",
+	"TR": "table",
+	"COLGROUP": "table tr",
+	"COL: ": "table tr",
+	"TH": "table tr",
+	"TD": "table tr",
+	"LI": "ul",
+	"DT": "dl",
+	"DD": "dl",
+	"OPTION": "select",
+	"OPTGROUP": "select",
+	"AREA": "map",
+	"LEGEND": "fieldset",
+};
+
+const _D7_CACHE_HTML = {};
+class D7Renderer {
+	constructor(path) {
+		this._META = null;
+		this._VIRTUAL_DOM = null;
+	}
+
+	static create = async function(path) {
+		let htmlText = _D7_CACHE_HTML[path];
+		if (!htmlText) {
+			htmlText = await D7Api.loadHtml(_D7_PAGE_BASE + path);
+		}
+
+		const d7Renderer = new D7Renderer();
+		d7Renderer._META = D7Renderer.compile(htmlText);
+		d7Renderer._VIRTUAL_DOM = D7Renderer.makeContainer(d7Renderer._META.virtualTag);
+
+		return d7Renderer;
+	}
+
+	render = function(model) {
+		this._VIRTUAL_DOM.innerHTML = this._META.fnRender(model);
+		return this._VIRTUAL_DOM;
+	}
+
+	renderLayoutElem = function(model) {
+		return null;
+	}
+
+	static compile(htmlText) {
+		return {
+			fnRender: function(_m) {return '<div>ok</div>'},
+			fnRenderLayoutInfo: function(_m) {return "<div d7Layout=''>layoutA</div>"},
+			virtualTag: 'div',
+		};
+	}
+
+	static analyze(htmlText) {
+		return {
+			css: '',
+			html: '',
+			script: '',
+			layoutElement: '',
+		};
+	}
+
+	static makeContainer = function(tagName) {
+		var divTmp = document.createElement("div");
+		var strContainer = _D7_CONTAINER_TYPE[tagName.toUpperCase()];
+		if (!strContainer) return divTmp;
+
+		var tags = strContainer.split(" ");
+		for (var idx in tags) {
+			var tempTag = document.createElement(tags[idx]);
+			divTmp.append(tempTag);
+			divTmp = tempTag;
+		}
+		return divTmp;
+	}
+}
+/*******************************
+ * UTIL
+ *******************************/
 class D7Util {
 	static debug = function(data) {
 		console.log(data);
 	}
+	static equals = function(srcData, tarData) {
+		return false;
+	}
 	static parseUrl = function(strUrl) {
-		var params = {};
-		strUrl = strUrl.startsWith("http:") ? strUrl : ("http://tmp.com" + strUrl);
+		var query = {};
+		strUrl = strUrl.startsWith("http") ? strUrl : ("http://tmp.com" + strUrl);
 		const url = new URL(strUrl);
 		(new URLSearchParams(url.search)).forEach(function(value, key) {
-			params[key] = value;
+			query[key] = value;
 		});
-		return {url:'', path:'', query:params};
+		return {path: url.pathname, query: query, origin: url.origin};
 	}
 }
 class D7Api {
-	static loadComp = (compUrl) => {
+	static loadHtml = (htmlUrl) => {
 		return new Promise((resolve, reject) => {
 			let request = new XMLHttpRequest();
-			request.open('GET', compUrl);
+			request.open('GET', htmlUrl);
 	
 			request.onload = function() {
 				if (request.status == 200) {
 					resolve(request.responseText);
 				} else {
-					reject(`http: ${compUrl} status[${request.status}]`);
+					reject(`http: ${htmlUrl} status[${request.status}]`);
 				}
 			}
 			request.onerror = function() {
-				reject(`http: ${compUrl} status[${request.status}]`);
+				reject(`http: ${htmlUrl} status[${request.status}]`);
 			}
 			request.send();
 		});
@@ -177,11 +345,28 @@ class D7Api {
 	}
 }
 
-const fn = Dinosaur7.prototype;
+/*******************************
+ * RUNTIME
+ *******************************/
+const fn = D7App.prototype;
 fn.util = D7Util;
 fn.api = D7Api;
-
-const d7 = new Dinosaur7();
+const d7App = new D7App();
 (function (global) {
-	d7.showPage(window.location.href);
+	switch (document.readyState) {
+		case "loading":
+			document.addEventListener('DOMContentLoaded', function () {
+				d7App.start();
+			});
+			/*/ after image css loaded.
+			window.addEventListener("load", function () {
+				d7App.start();
+			});
+			*/
+			break;
+		default : // [interactive, complete]
+			d7App.start();
+			break;
+	}
 })(this);
+
