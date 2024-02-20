c665a849323a98d649ca2863a8b9274e12247c60
cycau <116693177+cycau@users.noreply.github.com>
2024-02-20 15:27:04 +0900

    Update ApiController.java

--- d7server/controller/ApiController.java
+++ d7server/controller/ApiController.java
@@ -1,26 +1,75 @@
 package com.cycau.d7server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
     @Value("${d7server.devmode}")
     private boolean devmode;
-    @Value("${d7server.resource.root}")
-    private String resourceRoot;
+    @Autowired
+    private MockData mockData;
 
-    @GetMapping("/**")
-    public ResponseEntity<Object> get(HttpServletRequest request) {
-        if (devmode) return MockData.getResponse(request);
+    @RequestMapping("/**")
+    public ResponseEntity<Object> get(HttpServletRequest request) throws IOException {
+    	Map<String, Object> httpRequest = collectRequestData(request);
+        if (devmode) {
+        	try {
+				return mockData.getResponse(request.getMethod(), request.getRequestURI(), httpRequest);
+			} catch (IOException e) {
+				System.out.println("[ERROR] Something wrong.");
+		        return new ResponseEntity<>("Something wrong.", HttpStatus.FORBIDDEN);
+			}
+        }
 
-        return new ResponseEntity<>("Not support live mode yet.", HttpStatus.OK);
+        return new ResponseEntity<>("Does not support live mode yet.", HttpStatus.OK);
+    }
+
+    private Map<String, Object> collectRequestData(HttpServletRequest req) throws IOException {
+        Map<String, Object> request = new HashMap<>();
+        Map<String, Object> header = new HashMap<>();
+        Map<String, Object> query = new HashMap<>();
+
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            header.put(name, req.getHeader(name));
+        }
+        // query + formData
+        Enumeration<String> queryNames = req.getParameterNames();
+        while (queryNames.hasMoreElements()) {
+            String name = queryNames.nextElement();
+            query.put(name, req.getParameter(name));
+        }
+        if (((String)header.getOrDefault("Content-Type", "")).contains("json")) {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                Map<String, Object> data = mapper.readValue(req.getInputStream(), new TypeReference<Map<String, Object>>(){});
+                request.put("data", data);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+        	request.put("data", new String(req.getInputStream().readAllBytes()));
+        }
+
+        request.put("header", header);
+        request.put("query", query);
+        return request;
     }
 }
